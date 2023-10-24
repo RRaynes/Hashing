27b653902050fdb20c771848aa7dd270b4e8c06a
areyn <areyn@64.93.165.181>
2023-10-24 18:13:19 -0400

    simple hash table with tester

--- Hashing/src/camhash/SimpleHashTable.java
+++ Hashing/src/camhash/SimpleHashTable.java
@@ -3,22 +3,22 @@ package camhash;
 public class SimpleHashTable {
 	int[] content;
 	boolean[] occupied;
-	
+
 	public SimpleHashTable(int size) {
 		content = new int[size];
 		occupied = new boolean[size];
 	}
-	
+
 	public SimpleHashTable() {
 		this(20);
 	}
-	
-	//returns hash index given a key using key%table size
+
+	// returns hash index given a key using key%table size
 	int hash(int key) {
-		return key%content.length;
+		return key % content.length;
 	}
-	
-	//inserts if the position is available, returns false if it is already occupied
+
+	// inserts if the position is available, returns false if it is already occupied
 	boolean insert(int key) {
 		int index = hash(key);
 		if (occupied[index] == true)
@@ -27,22 +27,41 @@ public class SimpleHashTable {
 		occupied[index] = true;
 		return true;
 	}
-	
-	//searches for 'key' returns index, if not there, returns -1
+
+	// searches for 'key' returns index, if not there, returns -1
 	int search(int key) {
 		int index = hash(key);
-		if (occupied[index] == true && content[index] == key) //only condition that means the key is present
+		if (occupied[index] == true && content[index] == key) // only condition that means the key is present
 			return index;
-		return -1; //otherwise return -1
+		return -1; // otherwise return -1
 	}
-	
-	//searches for 'key' at index, if its there deletes it, if not does nothing
+
+	// searches for 'key' at index, if its there deletes it, if not does nothing
 	boolean delete(int key) {
 		int index = hash(key);
-		if (occupied[index] == true && content[index] == key) { //condition where the key is found
+		if (occupied[index] == true && content[index] == key) { // condition where the key is found
 			occupied[index] = false;
 			return true;
-		} 
+		}
 		return false;
+	}
+
+	void print() {
+		System.out.print("Index:\t\t");
+		for (int i = 0; i < content.length; i++) {
+			System.out.print(i + "\t");
+		}
+		System.out.print("\nContent:\t");
+		for (int i : content) {
+			System.out.print(i + "\t");
+		}
+		System.out.print("\nOccupied:\t");
+		for (boolean b : occupied) {
+			if (b)
+				System.out.print("T" + "\t");
+			else
+				System.out.print("F" + "\t");
+		}
+		System.out.println("\n");
 	}
 }
--- Hashing/src/camhash/Tester.java
+++ Hashing/src/camhash/Tester.java
@@ -1,5 +1,39 @@
 package camhash;
 
 public class Tester {
-
+	public static void main(String[] args) {
+		SimpleHashTable t1 = new SimpleHashTable(11);
+		System.out.println("2.1 - Simple Hash Table creaeted 't1' with size 11");
+		System.out.println("2.2 - Inserting '34, 29, 53, 44, 120, 39, 45, 40' into the hash table and displaying after each insertion");
+		t1.insert(34);
+		t1.print();
+		t1.insert(29);
+		t1.print();
+		t1.insert(53);
+		t1.print();
+		t1.insert(44);
+		t1.print();
+		t1.insert(120);
+		t1.print();
+		t1.insert(39);
+		t1.print();
+		t1.insert(45);
+		t1.print();
+		t1.insert(40);
+		t1.print();
+		
+		System.out.println("\n2.3 - Deleting '120' and displaying hash table | result: " + t1.delete(120));
+		t1.print();
+		
+		System.out.println("\n2.4 - Inserting '40' and displaying hash table | result: " + t1.insert(40));
+		t1.print();
+		
+		System.out.println("\n2.5 - Inserting '46' and displaying hash table | result: " + t1.insert(46));
+		t1.print();
+		
+		System.out.println("----------------------------------------------------------------------------------------------------");
+		
+		System.out.println("\n3.1");
+	}
+	
 }
